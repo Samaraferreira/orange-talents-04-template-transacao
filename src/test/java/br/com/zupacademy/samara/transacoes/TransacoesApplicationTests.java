15627da4ccd50d8f2514110ebccb30dc6f527394
Samara Ferreira <ferreirasamara.sf@gmail.com>
2021-05-19 16:36:58 -0300

    chore: setup inicial

---
+++ src/test/java/br/com/zupacademy/samara/transacoes/TransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.samara.transacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
