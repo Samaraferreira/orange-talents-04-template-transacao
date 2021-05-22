633eaacd1adfa06d010f13e8cf42945c3acf0168
Samara Ferreira <ferreirasamara.sf@gmail.com>
2021-05-21 22:55:51 -0300

    feat: autenticação e autorização

--- src/main/java/br/com/zupacademy/samara/transacoes/transacao/TransacaoController.java
+++ src/main/java/br/com/zupacademy/samara/transacoes/transacao/TransacaoController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/transacoes")
+@RequestMapping("/api/transacoes")
 public class TransacaoController {
 
     @Autowired
